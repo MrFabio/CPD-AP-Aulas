13e52844cfa8a8392f066b76aca7f57c809ae75d
Fabio Gomes mrfabiogomes@gmail.com <mrfabiogmes@gmail.com>
2015-03-18 00:55:41 +0000

    Final script and version 1

--- 2-oddeven/script_times.sh
+++ 2-oddeven/script_times.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-for th in 2 4 6 8 10 12 16 18 20 30 40
+for th in 2 4 6 8 10 
 do
 
 
 
 export OMP_NUM_THREADS=$th
-for size in 500 1000 5000 10000 50000 100000 #500000 #1000000 5000000 10000000 5000000
+for size in 500 1000 5000 10000 50000 100000 
 do
 
 echo "1-  $size"
